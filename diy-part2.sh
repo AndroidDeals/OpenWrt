b45cbfb44e1d840e0c0bc09ffd48f965908f3750
AndroidDeals <34513334+AndroidDeals@users.noreply.github.com>
2021-12-21 21:18:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/BoringCat/minieap-openwrt.git package/minieap
-git clone https://github.com/BoringCat/luci-app-minieap.git package/luci-app-minieap
 git clone https://github.com/vernesong/OpenClash.git && cp -r OpenClash/luci-app-openclash package/luci-app-openclash
